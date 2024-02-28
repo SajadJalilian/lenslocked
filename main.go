4f07e85aa087cd3f8a9b98953a2973f305f5a317
Sajad Jalilian <sajadjalilian88@gmail.com>
2024-02-28 19:47:22 +0330

    Add another route

--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Contact me via this email...!</h1><br><a href=\"/\">Home</a>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
