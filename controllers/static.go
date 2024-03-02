bf028c399a9d6713186616a79920b7c3188c6acd
Sajad Jalilian <sajadjalilian88@gmail.com>
2024-03-02 16:38:33 +0330

    Add dynamic question and answers to FAQ page

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/sajadjalilian/lenslocked/views"
@@ -8,6 +9,34 @@ import (
 
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		tpl.Execute(w)
+		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "First Q",
+			Answer:   "First Q answer",
+		},
+		{
+			Question: "Second Q",
+			Answer:   "Second Q answer",
+		},
+		{
+			Question: "Third Q",
+			Answer:   "Third Q answer",
+		},
+		{
+			Question: "What is your email?",
+			Answer:   `<a href="mailto:sajadjalilian88@gmail.com">sajadjalilian88@gmail.com</a>`,
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
--- views/template.go
+++ views/template.go
@@ -39,10 +39,10 @@ type Template struct {
 	htmlTpl *template.Template
 }
 
-func (t *Template) Execute(w http.ResponseWriter) {
+func (t *Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	err := t.htmlTpl.Execute(w, nil)
+	err := t.htmlTpl.Execute(w, data)
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
