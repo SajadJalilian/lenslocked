26a43acf91907f7f89ddc1aa0f6cbd44d158cdaf
Sajad Jalilian <sajadjalilian88@gmail.com>
2024-03-03 22:46:50 +0330

    Update create user method

--- controllers/users.go
+++ controllers/users.go
@@ -16,5 +16,6 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Temp res")
+	fmt.Fprint(w, "Email:", r.FormValue("email"))
+	fmt.Fprint(w, "Password:", r.FormValue("password"))
 }
