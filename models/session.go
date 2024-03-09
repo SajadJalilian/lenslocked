ba0ff82aca6cbe6d859f609c5618bcde43a42898
Sajad Jalilian <sajadjalilian88@gmail.com>
2024-03-09 22:37:43 +0330

    Add session service Create and User methods

--- models/session.go
+++ models/session.go
@@ -1,7 +1,28 @@
 package models
 
+import "database/sql"
+
 type Session struct {
-	ID        int
+	ID int
+	// Token is only set when creating a new session. When look un a session
+	// this will be left empty. as we only store the hash of a session token
+	// in our database and we cannot reverse it into a raw token
+	Token     string
 	UserId    int
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	// TODO: Create the session token
+	// TODO: Implement SessionService.Create
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	// TODO: Implement SessionService.User
+	return nil, nil
 }
