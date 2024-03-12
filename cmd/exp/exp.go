f12b820d9e13e1a6e0f876de10f5ac596fd85a7a
Sajad Jalilian <sajadjalilian88@gmail.com>
2024-03-12 21:33:21 +0330

    Creating context package

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,24 +1,4 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
-
 func main() {
-	err := CreateUser()
-	fmt.Println(err)
-}
-
-func Connect() error {
-	return errors.New("connection failed")
-}
-
-func CreateUser() error {
-	err := Connect()
-	if err != nil {
-		return fmt.Errorf("create user: %w", err)
-	}
-
-	return nil
 }
